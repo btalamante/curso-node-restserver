d8615537af0c98eebe08d60fa7557439128444bf
BrauZ <braulionm@hotmail.com>
2021-06-25 19:12:18 -0500

    Primer commit

---
+++ controllers/user.js
@@ -0,0 +1,47 @@
+const { response, request } = require('express');
+
+
+
+const usuariosGet = (req = request, res = response) => {
+    const query = req.query;
+    res.json({
+        msg: 'get API - controlador',
+        query
+    });
+}
+
+const usuariosPut = (req = request, res = response) => {
+    const id = req.params.id;
+    res.json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosPost = (req = request, res = response) => {
+    const body = req.body;
+    res.json({
+        msg: 'post API - controlador',
+        body
+    });
+}
+
+const usuariosPatch = (req = request, res = response) => {
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+const usuariosDelete = (req = request, res = response) => {
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosPatch,
+    usuariosDelete
+}
