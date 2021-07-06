8b20f7c459e0f33925de26df59e980bf58d91864
BrauZ <braulionm@hotmail.com>
2021-07-06 11:24:46 -0500

    Google Signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
+const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
@@ -38,6 +39,48 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSiginin = async (req = request, res = response) => {
+    const { id_token } = req.body;
+    // console.log(id_token);
+    // console.log(req);
+    const { correo, nombre, img } = await googleVerify(id_token);
+    //Usuario de Google verificado correctamente
+    //console.log(googleUser);
+
+    try {
+        let usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            //Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+            usuario = new Usuario(data);
+            //console.log(usuario);
+            await usuario.save();
+        }
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+        const token = await generarJWT(usuario.id);
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es reconocido'
+        });
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSiginin
 }
